80f225dfec219d4c8783ba9d9eee367520337687
Kay <kehiiiiya@gmail.com>
2023-12-25 18:18:57 +0000

    chore: adding detail for new functions

--- lib/coin_utils.go
+++ lib/coin_utils.go
@@ -2,10 +2,12 @@ package lib
 
 const changeFactor = float64(1000000000)
 
+// converting PAC coin amount to satoshi amount.
 func PACToSatoshiPAC(coin float64) int64 {
 	return int64(coin * changeFactor)
 }
 
+// converting satoshi amount to PAC coin amount.
 func SatoshiPACtoPAC(change int64) float64 {
 	return float64(change) / changeFactor
 }
